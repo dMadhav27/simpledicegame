254f3b4f333ff920f929a1efe58ac95b140c651a
dMadhav27 <120191817+dMadhav27@users.noreply.github.com>
2023-08-01 18:52:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomimagesrc = "./images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomimagesrc);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber2 + ".png";
+var randomimagesrc = "./images/" + randomDiceImage;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomimagesrc);
+
+
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} 
+    else if (randomNumber2>randomNumber1){
+        document.querySelector("h1").innerHTML = "Player 2 Wins!!";
+    }
+
+    else {
+        document.querySelector("h1").innerHTML = "There's a tie!";
+    }
